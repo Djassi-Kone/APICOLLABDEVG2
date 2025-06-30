4387b3c7f2d31dd71a4257bc579dc26e868944ee
Djassi-Kone <150795696+Djassi-Kone@users.noreply.github.com>
2025-06-30 14:00:01 +0000

    la structure du projet

---
+++ src/main/java/groupe2/apicollabdevg2/Apicollabdevg2Application.java
@@ -0,0 +1,13 @@
+package groupe2.apicollabdevg2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apicollabdevg2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apicollabdevg2Application.class, args);
+	}
+
+}
---
+++ src/test/java/groupe2/apicollabdevg2/Apicollabdevg2ApplicationTests.java
@@ -0,0 +1,13 @@
+package groupe2.apicollabdevg2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apicollabdevg2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
